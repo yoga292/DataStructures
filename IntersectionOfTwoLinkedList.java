13dcce7e4451b4a4ab8ba0f683d77c3225cca918
yoga292 <yogapriyavision@gmail.com>
2025-06-18 10:07:50 +0530

    Create IntersectionOfTwoLinkedList.java

---
+++ IntersectionOfTwoLinkedList.java
@@ -0,0 +1,23 @@
+class ListNode {
+    int val;
+    ListNode next;
+    
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class IntersectionFinder {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode pointerA = headA;
+        ListNode pointerB = headB;
+        while (pointerA != pointerB) {
+            pointerA = (pointerA != null) ? pointerA.next : headB;
+            pointerB = (pointerB != null) ? pointerB.next : headA;
+        }
+        return pointerA;
+    }
+}
