6b91e8120ff3a4a455a9a4e3e12b402677296578
yoga292 <yogapriyavision@gmail.com>
2025-06-22 21:17:09 +0530

    Create CoinChange.java

---
+++ CoinChange.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1; 
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max); 
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] == max ? -1 : dp[amount];
+    }
+}
