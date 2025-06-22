aed277c006debf151713db043aa1e86523efd29d
yoga292 <yogapriyavision@gmail.com>
2025-06-22 20:46:51 +0530

    Create 01Knapsack.java

---
+++ 01Knapsack.java
@@ -0,0 +1,16 @@
+class Solution {
+    static int knapsack(int W, int val[], int wt[]) {
+        int n=val.length;
+        int[][] dp=new int[n+1][W+1];
+        for(int i=1;i<=n;i++){
+            for(int w=1;w<=W;w++){
+                if(wt[i-1]<=w){
+                    dp[i][w]=Math.max(dp[i-1][w],val[i-1]+dp[i-1][w-wt[i-1]]);
+                }else{
+                    dp[i][w]=dp[i-1][w];
+                }
+            }
+        }
+        return dp[n][W];
+    }
+}
